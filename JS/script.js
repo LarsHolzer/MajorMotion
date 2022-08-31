778160aa3a589196241dc553fb96731d3e7b6657
LarsHolzer <108394052+LarsHolzer@users.noreply.github.com>
2022-08-31 15:52:54 +0200

    Fixed toggle Menu

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,5 @@
-const menuToggle = document.querySelector(".toggle")
-const showcase = document.querySelector(".showcase")
 
-menuToggle.addEventListener("click", () => {
-    menuToggle.classList.toggle("active")
-    showcase.classList.toggle("active")
-})
+// Open Contact Formular
 
 function openForm() {
     document.getElementById("myForm").style.display = "block";
@@ -13,3 +8,19 @@ function openForm() {
 function closeForm() {
     document.getElementById("myForm").style.display = "none";
   }
+
+// fade in Contact form
+
+let myForm = document.getElementById("myForm")
+document.getElementById("open-button").onclick = function(){
+  myLogo.classList.toggle("fade")
+}
+
+//open nav 2
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
